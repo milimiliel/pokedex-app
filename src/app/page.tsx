b3d87894785bd2ba2d934d7723b79274f63ff581
Vitor Gouvea <gouveavitor.2013@gmail.com>
2023-06-11 19:02:08 -0300

    initial css and routing experiments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
-import styles from "./page.module.css";
-
 export default function Home() {
-  return <main className={styles.main}>oi</main>;
+  return (
+    <main>
+      <div>oi</div>
+    </main>
+  );
 }
---
+++ src/app/pokedex/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Page() {
+  return <div>pokedex</div>;
+}
+
+export default Page;
