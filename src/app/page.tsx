0b34b5eef468e68c73ed3721a2842ab779746a0c
Vitor Gouvea <gouveavitor.2013@gmail.com>
2023-06-12 08:18:27 -0300

    add header & icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Header from "@/components/Header/Header";
+
 export default function Home() {
   return (
     <main>
-      <div>oi</div>
+      <div>
+        <Header />
+      </div>
     </main>
   );
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./header.sass";
+
+function Header() {
+  return (
+    <header>
+      <img src="pokeball-icon-hd.png" />
+      <h1 className="header-h1">Pokédex</h1>
+    </header>
+  );
+}
+
+export default Header;
